e3ae833d61dca5a622a4c77fe12f3fe85620a041
Lucas <luke.difalco52@gmail.com>
2021-11-01 14:05:52 -0400

    created teaser, debugged word method, removed debug message

--- play.rb
+++ play.rb
@@ -1,8 +1,8 @@
 class Hangman
 
     def initialize 
-        letters = ('a'..'z').to_a
-        word = words.sample
+        @letters = ('a'..'z').to_a
+        @word = words.sample
     end
 
     def words
@@ -19,4 +19,24 @@ class Hangman
 
     def begin
         #starts game asking for letter
+        puts "New game started... your word is #{ @word.first.size } characters long"
+        word_teaser = ""
+        
+        @word.first.size.times do
+            word_teaser += "_ "
+        end
+    
+
+        puts word_teaser
+        puts "Clue: #{ @word.last }"
+
+        puts "Enter a letter"
+        guess = gets.chomp
+
+
+    end
 end
+
+game = Hangman.new
+game.begin
+
